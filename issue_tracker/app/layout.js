5cac67d102d0d93d4968634ba3747740ea6fdfd5
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-10-21 15:34:24 +0300

    Structure Layout

--- issue_tracker/app/layout.js
+++ issue_tracker/app/layout.js
@@ -1,16 +1,34 @@
+"use client";
 import "./globals.css";
-
-export const metadata = {
-  title: "Issue Tracker",
-  description: "Track issues with no hassle",
-};
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
+// export const metadata = {
+//   title: "Issue Tracker",
+//   description: "Track issues with no hassle",
+// };
 
 export default function RootLayout({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
+      <body className="flex">
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+        <main className="flex flex-col w-full">
+          <Navbar toggleSidebar={toggleSidebar} />
+          <div className="px-8 py-10">{children}</div>
+        </main>
       </body>
     </html>
   );
 }
+
+/**
+ *
+ * Note:
+ * Root layout should always be rendered from the server side. Therefore find a way
+ * to not useState
+ */

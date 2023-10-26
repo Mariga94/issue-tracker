a08a03fc983296058dc0361b27c22fde69b2398f
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-10-26 16:13:15 +0300

    Add changes to navbar

--- issue_tracker/components/Navbar.jsx
+++ issue_tracker/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import ProfilePhoto from "./ProfilePhoto";
-const Navbar = ({ toggleSidebar }) => {
+const Navbar = ({
+  toggleSidebar,
+  toggleDisplayIconsOnly,
+  displayIconsOnly,
+}) => {
+  const [showProfileModal, setShowProfileModal] = useState(true);
+
+  const handleSetShowProfileModal = () => {
+    setShowProfileModal(!showProfileModal);
+  };
+  const closeModal = () => {
+    setShowProfileModal(false);
+  };
+
   return (
-    <nav className="h-[63px] min-w-full border-b-[1px] border-b-[#E5E7EB] flex flex-row items-center  px-8 cursor-pointer justify-between lg:justify-end">
+    <nav className="h-[63px] min-w-full border-b-[1px] border-b-[#E5E7EB] flex flex-row items-center  px-8 cursor-pointer justify-between lg:justify-between">
       <button className="icon lg:hidden" onClick={toggleSidebar}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +33,48 @@ const Navbar = ({ toggleSidebar }) => {
           />
         </svg>
       </button>
-      <div className="flex flex-row gap-3">
+      {!displayIconsOnly ? (
+        <button
+          className="icon hidden lg:block"
+          onClick={toggleDisplayIconsOnly}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </button>
+      ) : (
+        <button
+          className="icon hidden lg:block"
+          onClick={toggleDisplayIconsOnly}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </button>
+      )}
+      <div className="flex flex-row gap-10 items-center">
         <div className="text-[#9ca3af] flex">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -36,11 +91,59 @@ const Navbar = ({ toggleSidebar }) => {
             />
           </svg>
         </div>
-        <ProfilePhoto />
-        <div>
-          <span className="regular-14 leading-normal">John Doe</span>
+        <div className="">
+          <Menu as="div" className="relative">
+            <div>
+              <Menu.Button className="">
+                <div className="flex flex-row gap-2 items-center">
+                  <ProfilePhoto />
+                  <span className=" lg:inline-block regular-14 leading-normal sm:hidden">
+                    John Doe
+                  </span>
+                </div>
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Profile
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                  {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Sign Out
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
+       
       </div>
+      
     </nav>
   );
 };
